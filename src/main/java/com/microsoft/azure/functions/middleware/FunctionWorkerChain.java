90e94cfd85231d2fa1c6cb6a49826474cec76cf7
kaibocai <89094811+kaibocai@users.noreply.github.com>
2022-09-02 14:05:01 -0700

    remove FunctionalInterface

--- src/main/java/com/microsoft/azure/functions/middleware/FunctionWorkerChain.java
+++ src/main/java/com/microsoft/azure/functions/middleware/FunctionWorkerChain.java
@@ -2,7 +2,6 @@ package com.microsoft.azure.functions.middleware;
 
 import com.microsoft.azure.functions.ExecutionContext;
 
-@FunctionalInterface
 public interface FunctionWorkerChain {
     public void doNext(ExecutionContext context);
 }
--- src/main/java/com/microsoft/azure/functions/middleware/FunctionWorkerMiddleware.java
+++ src/main/java/com/microsoft/azure/functions/middleware/FunctionWorkerMiddleware.java
@@ -2,7 +2,6 @@ package com.microsoft.azure.functions.middleware;
 
 import com.microsoft.azure.functions.ExecutionContext;
 
-@FunctionalInterface
 public interface FunctionWorkerMiddleware {
     public void invoke (ExecutionContext context, FunctionWorkerChain next);
 }
