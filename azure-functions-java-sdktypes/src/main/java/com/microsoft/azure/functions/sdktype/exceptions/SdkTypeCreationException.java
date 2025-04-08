238c3cb370c73c2c1a0458ae047bdc5f96ac4947
Ahmed Muhsin <ahmedmuhsin@microsoft.com>
2025-04-08 11:48:30 -0500

    fix doc issues

--- azure-functions-java-sdktypes/src/main/java/com/microsoft/azure/functions/sdktype/exceptions/SdkTypeCreationException.java
+++ azure-functions-java-sdktypes/src/main/java/com/microsoft/azure/functions/sdktype/exceptions/SdkTypeCreationException.java
@@ -1,7 +1,7 @@
 package com.microsoft.azure.functions.sdktype.exceptions;
 
 /**
- * Thrown by SdkTypeFactory or SdkType<?> if building the final SdkType fails.
+ * Thrown by SdkTypeFactory or SdkType if building the final SdkType fails.
  */
 public class SdkTypeCreationException extends RuntimeException {
     public SdkTypeCreationException(String message) {
